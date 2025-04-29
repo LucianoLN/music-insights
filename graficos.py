89b1b0735132ec4f8eb4e4227bcc7bc23fb56ffa
Luciano Nascimento <luciano.souza.nascimento.filho@ccc.ufcg.edu.br>
2025-04-29 00:53:39 -0300

    feat: exploração das bibliotecas

---
+++ graficos.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+url = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv"
+
+df = pd.read_csv(url)
+
+generos_contagem = df['playlist_genre'].value_counts().head(10)
+
+
+
+
+
+plt.figure(figsize=(12, 6))
+
+sns.barplot(x=generos_contagem.values, y=generos_contagem.index)
+
+plt.title('Top 10 Gêneros com Mais Músicas no Dataset', fontsize=16)
+plt.xlabel('Número de Músicas')
+plt.ylabel('Gênero Musical')
+
+plt.tight_layout()
+plt.savefig("graficos/grafico_generos.png")
